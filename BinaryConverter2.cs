30cc640ddc73d41c6bdb1254409568149b8f869b
TroyMateo <48836188+TroyMateo@users.noreply.github.com>
2019-05-19 15:53:25 -0700

    Add files via upload
    Provides another way to convert integers to binary value

---
+++ BinaryConverter2.cs
@@ -0,0 +1,36 @@
+﻿/*Author: Troy Mateo
+ * Date: 05/10/2019
+ * C# program to convert integers in binary values
+ */
+using System;
+
+namespace BinaryConverter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Request user input for the desired number they want to convert
+            Console.Write("Please enter number to be converted: ");
+            string input = Console.ReadLine();
+
+            //Provides variable that will be assigned a value if TryParse is successful
+            int number;
+            if (int.TryParse(input, out number))
+            {
+
+            }
+            else
+            {
+                //Error message if an invalid number is input
+                Console.WriteLine("Sorry that wasn't a valid number");
+            }
+
+            //Evaluates number to base 2
+            string binary = Convert.ToString(number, 2);
+            
+            //Prints out the binary result
+            Console.WriteLine("Binary: {0}", binary);
+        }
+    }
+}
